42d16b05f713ae7aa61e2835b1ea74058a11f972
C.Rondeau <rondeau.clement@gmail.com>
2021-04-06 15:00:36 +0200

    + implement SFX type and its inspector

---
+++ Assets/Packages/Cake/Opera/Editor/SFXEditor.cs
@@ -0,0 +1,85 @@
+using UnityEditor;
+
+namespace Cake.Opera
+{
+    [CustomEditor(typeof(SFX))]
+    [CanEditMultipleObjects]
+    public class SFXEditor : Editor
+    {
+        // Clip(s)
+        private SerializedProperty m_random;
+        private SerializedProperty m_clip;
+        private SerializedProperty m_clips;
+
+        // Pitch
+        private SerializedProperty m_pitchRandom;
+        private SerializedProperty m_pitch;
+        private SerializedProperty m_pitchRange;
+
+        // Volume
+        private SerializedProperty m_volumeRandom;
+        private SerializedProperty m_volume;
+        private SerializedProperty m_volumeRange;
+
+        void OnEnable()
+        {
+            // Clip(s)
+            m_random = serializedObject.FindProperty("Random");
+            m_clip = serializedObject.FindProperty("Clip");
+            m_clips = serializedObject.FindProperty("Clips");
+
+            // Pitch
+            m_pitchRandom = serializedObject.FindProperty("PitchRandom");
+            m_pitch = serializedObject.FindProperty("Pitch");
+            m_pitchRange = serializedObject.FindProperty("PitchRange");
+
+            // Volume
+            m_volumeRandom = serializedObject.FindProperty("VolumeRandom");
+            m_volume = serializedObject.FindProperty("Volume");
+            m_volumeRange = serializedObject.FindProperty("VolumeRange");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            // Clip(s)
+            EditorGUILayout.PropertyField(m_random);
+
+            if (m_random.boolValue)
+            {
+                EditorGUILayout.PropertyField(m_clips);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(m_clip);
+            }
+
+            // Pitch
+            EditorGUILayout.PropertyField(m_pitchRandom);
+
+            if (m_pitchRandom.boolValue)
+            {
+                EditorGUILayout.PropertyField(m_pitchRange);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(m_pitch);
+            }
+
+            // Volume
+            EditorGUILayout.PropertyField(m_volumeRandom);
+
+            if (m_volumeRandom.boolValue)
+            {
+                EditorGUILayout.PropertyField(m_volumeRange);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(m_volume);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Packages/Cake/Opera/SFX.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Cake.Data;
+
+namespace Cake.Opera
+{
+    [CreateAssetMenu(fileName = "SFX", menuName = "Game/SFX", order = 1)]
+    public class SFX : ScriptableObject
+    {
+        // Clip(s)
+        public AudioClip Clip;
+        public bool Random;
+        public List<AudioClip> Clips;
+
+        // Volume
+        public bool VolumeRandom;
+        [Range(0f, 1f)]
+        public float Volume = 1f;
+        public ValueRange VolumeRange = new ValueRange(0f, 1f);
+
+        // Pitch
+        public bool PitchRandom;
+        [Range(0f, 1f)]
+        public float Pitch = 0.5f;
+        public ValueRange PitchRange = new ValueRange(0f, 1f);
+    }
+}
